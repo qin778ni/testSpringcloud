0a513c32ade7988288bc5f5a7090b27ee68b380f
hyq <ms.y.smileatlife@qq.com>
2018-09-10 17:01:21 +0800

    客户端的配置文件必须命名为bootstrap.properties,不然会出现配置无效问题

--- config-client/src/main/java/test/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/test/configclient/ConfigClientApplication.java
@@ -1,7 +1,10 @@
 package test.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ConfigClientApplication {
