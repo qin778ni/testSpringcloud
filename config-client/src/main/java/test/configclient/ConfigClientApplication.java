0e3f2136a4f5cc816263e6e030a8e8b0001aa27d
hyq <ms.y.smileatlife@qq.com>
2018-09-11 10:15:37 +0800

    将配置服务注册到注册中心

--- config-client/src/main/java/test/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/test/configclient/ConfigClientApplication.java
@@ -3,10 +3,14 @@ package test.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@ComponentScan
 public class ConfigClientApplication {
 
 	public static void main(String[] args) {
