75d6e961db1ae9b9cd09ac687f53915f92208ac4
hyq <ms.y.smileatlife@qq.com>
2018-09-12 15:10:12 +0800

    消息总线获取远程配置文件的更新数据

--- config-client/src/main/java/test/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/test/configclient/ConfigClientApplication.java
@@ -3,6 +3,7 @@ package test.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
--- config-client/src/main/java/test/configclient/controller/TestController.java
+++ config-client/src/main/java/test/configclient/controller/TestController.java
@@ -2,6 +2,7 @@ package test.configclient.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @create 2018/9/10-16:23
  */
 @RestController
+@RefreshScope
 public class TestController {
 
     @Value("${foo}")
